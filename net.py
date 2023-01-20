3b763b3988242dd262ea445d60187a8320095216
codezzzsleep <codezzzsleep@163.com>
2023-01-20 16:23:34 +0800

    网络模型搭建

---
+++ net.py
@@ -0,0 +1,45 @@
+import torch
+from torch import nn
+
+
+# 定义一个网络模型
+class MyLeNet5(nn.Module):
+    # 初始化网络
+    def __init__(self):
+        super(MyLeNet5, self).__init__()
+        # 卷积层
+        self.c1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2)
+        self.Sigmoid = nn.Sigmoid()
+        # 池化层
+        self.s2 = nn.AvgPool2d(kernel_size=2, stride=2)
+        # 卷积层
+        self.c3 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        # 池化层
+        self.s4 = nn.AvgPool2d(kernel_size=2, stride=2)
+        # 卷积层
+        self.c5 = nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5)
+
+        self.flatten = nn.Flatten()
+        # 线性连接层
+        self.f6 = nn.Linear(120, 84)
+        self.output = nn.Linear(84, 10)
+
+    # 传播
+    def forward(self, x):
+        x = self.Sigmoid(self.c1(x))
+        x = self.s2(x)
+        x = self.Sigmoid(self.c3(x))
+        x = self.s4(x)
+        x = self.c5(x)
+        x = self.flatten(x)
+        x = self.f6(x)
+        x = self.output
+
+        return x
+
+
+if __name__ == "__main__":
+    # 随机生成一个张量 批次为1， 通道为1， 图片大小为 28x28
+    x = torch.randn([1, 1, 28, 28])
+    model = MyLeNet5()
+    y = model(x)
